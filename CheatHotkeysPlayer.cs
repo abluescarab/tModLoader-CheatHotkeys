d0709470f2c63b0007b8201e2d49b71ea32b8da1
Alana Gilston <alana@abluescarab.us>
2016-12-22 00:38:41 -0800

    Fixed calls to PreHurt and PreKill

--- CheatHotkeysPlayer.cs
+++ CheatHotkeysPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 
 namespace CheatHotkeys {
@@ -8,20 +9,20 @@ namespace CheatHotkeys {
             return true;
         }
 
-        public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref string deathText) {
+        public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource) {
             if(((CheatHotkeys)mod).GodMode) {
                 return false;
             }
 
-            return base.PreHurt(pvp, quiet, ref damage, ref hitDirection, ref crit, ref customDamage, ref playSound, ref genGore, ref deathText);
+            return base.PreHurt(pvp, quiet, ref damage, ref hitDirection, ref crit, ref customDamage, ref playSound, ref genGore, ref damageSource);
         }
 
-        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref string deathText) {
+        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource) {
             if(((CheatHotkeys)mod).GodMode) {
                 return false;
             }
 
-            return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genGore, ref deathText);
+            return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genGore, ref damageSource);
         }
 
         public override void PreUpdateBuffs() {
