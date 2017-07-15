7f5e289411d72be0fab55975c57fa2c10bc23862
Alana Gilston <alana@abluescarab.us>
2017-07-15 07:59:00 -0700

    Added MiningBuffMode enum file

---
+++ MiningBuffMode.cs
@@ -0,0 +1,11 @@
+﻿using Terraria.ID;
+
+namespace CheatHotkeys {
+    public enum MiningBuffMode {
+        All = 0,
+        None = -1,
+        Dangersense = BuffID.Dangersense,
+        Hunter = BuffID.Hunter,
+        Spelunker = BuffID.Spelunker
+    }
+}
